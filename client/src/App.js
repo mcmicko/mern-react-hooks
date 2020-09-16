53d82ac4d2f3a01bc83914a2c4c9c9a7f3171c8d
mcmicko <mcmzrc@gmail.com>
2020-09-16 20:33:36 +0200

    Private Route

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 if (localStorage.token) setAuthToken(localStorage.token);
 
@@ -29,9 +31,8 @@ function App() {
             <Alert />
             <Switch>
               <Route exact path="/register" component={Register} />
-            </Switch>
-            <Switch>
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dasboard" component={Dashboard} />
             </Switch>
           </section>
         </React.Fragment>
